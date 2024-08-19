8781ed16a823c216da33caebe579e66809b599d2
shuheitachikawa <shuhei.tusx@gmail.com>
2024-08-19 15:55:18 +0900

    fix

--- src/app/admin/posts/[id]/page.tsx
+++ src/app/admin/posts/[id]/page.tsx
@@ -1,69 +1,74 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
-import { Category } from "@/app/_types/Category";
-import { Post } from "@/app/_types/Post";
-import Categories from "../../categories/page";
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import { Category } from '@/app/_types/Category'
+import { Post } from '@/app/_types/Post'
+import Categories from '../../categories/page'
+import { PostCategory } from '@prisma/client'
 
 const PostPage = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [thumbnailUrl, setThumbnailUrl] = useState("");
-  const [categories, setCategories] = useState<Category[]>([]);
-  const { id } = useParams();
-  const router = useRouter();
-  const [selectedCategories, setSelectedCategories] = useState<Category[]>([]);
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [thumbnailUrl, setThumbnailUrl] = useState('')
+  const [categories, setCategories] = useState<Category[]>([])
+  const { id } = useParams()
+  const router = useRouter()
+  const [selectedCategories, setSelectedCategories] = useState<Category[]>([])
 
   const handlePostSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
 
     //記事更新
     await fetch(`/api/admin/posts/${id}`, {
-      method: "PUT",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ title, content, thumbnailUrl, categories }),
-    });
-    alert("記事を更新しました。");
-    router.push("/admin/posts");
-  };
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ title, content, thumbnailUrl, categories: selectedCategories }),
+    })
+    alert('記事を更新しました。')
+    router.push('/admin/posts')
+  }
 
   //記事削除
   const handleDeletePost = async () => {
-    if (!confirm("記事を削除しますか？")) return;
+    if (!confirm('記事を削除しますか？')) return
     await fetch(`/api/admin/posts/${id}`, {
-      method: "DELETE",
-    });
-    alert("記事を削除しました。");
-    router.push("/admin/posts");
-  };
+      method: 'DELETE',
+    })
+    alert('記事を削除しました。')
+    router.push('/admin/posts')
+  }
 
   //記事を取得
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch(`/api/admin/posts/${id}`);
+      const res = await fetch(`/api/admin/posts/${id}`)
       // const data = await res.json();
       // console.log(data);
 
-      const { post } = await res.json();
-      setTitle(post.title);
-      setContent(post.content);
-      setThumbnailUrl(post.thumbnailUrl);
+      const { post } = await res.json()
+      setTitle(post.title)
+      setContent(post.content)
+      setThumbnailUrl(post.thumbnailUrl)
       // これは何をしていたか？
-      setSelectedCategories(post.postCategories.map((pc: Category) => pc.name));
-    };
-    fetcher();
-  }, [id, title]);
+      const postCategoris = post.postCategories
+      const categories: Category[] = postCategoris.map(
+        (postCategory: { category: Category }) => postCategory.category,
+      )
+      setSelectedCategories(categories)
+    }
+    fetcher()
+  }, [id])
 
   //カテゴリーを取得
   //  useEffect(()=>{});
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch("/api/admin/categories");
-      const { categories }: { categories: Category[] } = await res.json();
-      setCategories(categories);
-    };
-    fetcher();
-  }, []);
+      const res = await fetch('/api/admin/categories')
+      const { categories }: { categories: Category[] } = await res.json()
+      setCategories(categories)
+    }
+    fetcher()
+  }, [])
 
   // const handleSelectCategory = (category: Category) => {
   //   setSelectedCategories([...selectedCategories, category]);
@@ -71,14 +76,16 @@ const PostPage = () => {
   // console.log(selectedCategories);
 
   const handleSelectCategory = (category: Category) => {
-    if (selectedCategories.map((c) => c.id).includes(category.id)) {
+    const selectedCategoryIds = selectedCategories.map((c) => c.id)
+    const isSelected = selectedCategoryIds.includes(category.id)
+    if (isSelected) {
       setSelectedCategories(
-        selectedCategories.filter((c) => c.id !== category.id)
-      );
+        selectedCategories.filter((c) => c.id !== category.id),
+      )
     } else {
-      setSelectedCategories([...selectedCategories, category]);
+      setSelectedCategories([...selectedCategories, category])
     }
-  };
+  }
 
   return (
     <main className="w-4/5 mx-auto">
@@ -144,18 +151,18 @@ const PostPage = () => {
               //selectedCategories配列の中の一つのid
               .map((c) => c.id)
               //さらにcategory.idが含まれているもの
-              .includes(category.id);
+              .includes(category.id)
             return (
               <div
                 key={category.id}
                 onClick={() => handleSelectCategory(category)}
                 className={`${
-                  isSelected ? "bg-blue-500" : ""
+                  isSelected ? 'bg-blue-500' : ''
                 } border border-blue-500 w-fit p-1 rounded-md text-sm`}
               >
                 {category.name}
               </div>
-            );
+            )
           })}
         </div>
         <button
@@ -173,7 +180,7 @@ const PostPage = () => {
         </button>
       </form>
     </main>
-  );
-};
+  )
+}
 
-export default PostPage;
+export default PostPage
